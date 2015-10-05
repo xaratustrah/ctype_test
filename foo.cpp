905c5c3081b2e6cdc32970f70427053211af8862
xaratustrah <shahab@zamail.de>
2015-10-05 16:58:49 +0200

    added variables

--- foo.cpp
+++ foo.cpp
@@ -1,16 +1,41 @@
 // g++ -c -fPIC foo.cpp -o foo.o
 // g++ -shared -Wl,libfoo.so -o libfoo.so  foo.o
 
+/* Example to wrap C functions in python using ctypes
+
+   See http://stackoverflow.com/questions/145270/calling-c-c-from-python
+
+   ** C++ example file **
+   compile it as a shared lib with
+   g++ -c -fPIC foo.cpp -o foo.o
+   and
+   g++ -shared -Wl,-soname,libfoo.so -o libfoo.so  foo.o
+
+   Use it with Python wrapper at http://pastebin.com/0D700WPb
+
+*/
 
 #include <iostream>
 
 class Foo{
-public:
-     void bar(){
-	  std::cout << "Hello" << std::endl;
-     }
+    public:
+        void bar(){
+            std::cout << "Hello" << std::endl;
+        }
+        int doubleme (int input){
+	   std::cout << "Doubling " << input << std::endl;
+	   return 2*input;
+	}
+	void printfloat (double input){
+	   std::cout << "Printing a float " << input << std::endl;
+	}
+
 };
+
+
 extern "C" {
-     Foo* Foo_new(){ return new Foo(); }
-     void Foo_bar(Foo* foo){ foo->bar(); }
+    Foo* Foo_new(){ return new Foo(); }
+    void Foo_bar(Foo* foo){ foo->bar(); }
+    int  Foo_doubleme (Foo* foo, int input) {return foo->doubleme(input);}
+    void Foo_printfloat (Foo* foo, double input) {return foo->printfloat(input);}
 }
--- test.py
+++ test.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python
+
+# Example to wrap C functions in python using ctypes
+
+# See http://stackoverflow.com/questions/145270/calling-c-c-from-python
+
+# ** Python wrapper file **
+# To be used with example C++ file: http://pastebin.com/pyUXf3RW
+# Tested in Ubuntu 14.04
+
 from ctypes import cdll
+from ctypes import c_char_p
+from ctypes import c_double
+
 lib = cdll.LoadLibrary('./libfoo.so')
+
 
 class Foo(object):
     def __init__(self):
@@ -8,5 +22,16 @@ class Foo(object):
     def bar(self):
         lib.Foo_bar(self.obj)
 
-f= Foo()
+    def doubleme(self, intnumber):
+        return lib.Foo_doubleme(self.obj, intnumber)
+
+    def printfloat(self, doublenumber):
+        lib.Foo_printfloat(self.obj, c_double(doublenumber))
+
+
+f = Foo()
 f.bar()
+test = f.doubleme(4)
+print(test)
+f.printfloat(6.3)
+
